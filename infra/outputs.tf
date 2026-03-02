0f05a3362d80e0dbaee0723fa20137000966e041
Igor Popov <ip-adsense@yandex.ru>
2026-03-02 08:51:38 +0000

    change my-cluster to k8s-cluster

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,9 +1,9 @@
 output "k8s_external_v4_endpoint" {
-  value = yandex_kubernetes_cluster.my_cluster.master[0].external_v4_endpoint
+  value = yandex_kubernetes_cluster.k8s-cluster.master[0].external_v4_endpoint
 }
 
 output "k8s_ca_certificate" {
-  value = yandex_kubernetes_cluster.my_cluster.master[0].cluster_ca_certificate
+  value = yandex_kubernetes_cluster.k8s-cluster.master[0].cluster_ca_certificate
 }
 
 output "k8s_external_v4_address" {
