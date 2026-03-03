327f445d319f2844e6e14f33a978243dd83c84c5
Igor Popov <ip-adsense@yandex.ru>
2026-03-03 10:44:04 +0000

    fix dns.tf

--- infra/dns.tf
+++ infra/dns.tf
@@ -1,7 +1,7 @@
 # 1. Запрос на управляемый сертификат Let's Encrypt
 resource "yandex_cm_certificate" "le_cert" {
   name      = "sausage-store-le"
-  folder_id = var.yc_folder_id
+  folder_id = var.folder_id
 
   managed {
     challenge_type = "DNS_CNAME" # Самый надежный способ для автоматизации
@@ -19,7 +19,7 @@ resource "yandex_dns_zone" "sausage_store_public_zone" {
 # 3. АВТОМАТИЧЕСКАЯ ВАЛИДАЦИЯ
 # Terraform берет данные из запроса сертификата и создает запись в DNS
 resource "yandex_dns_recordset" "validation_record" {
-  zone_id = yandex_dns_zone.public_zone.id
+  zone_id = yandex_dns_zone.sausage_store_public_zone.id
   name    = yandex_cm_certificate.le_cert.challenges[0].dns_name
   type    = yandex_cm_certificate.le_cert.challenges[0].dns_type
   data    = [yandex_cm_certificate.le_cert.challenges[0].dns_value]
@@ -28,7 +28,7 @@ resource "yandex_dns_recordset" "validation_record" {
 
 resource "yandex_dns_recordset" "observability_records" {
   for_each = toset(["grafana", "jaeger", "pyroscope", "argocd", "k8s"])
-  zone_id  = yandex_dns_zone.public_zone.id
+  zone_id  = yandex_dns_zone.sausage_store_public_zone.id
   name     = "${each.value}.${var.domain_name}."
   type     = "A"
   ttl      = 600
