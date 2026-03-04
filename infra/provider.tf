7e4a02f56b02d90645264f301ebac2ce3bc0dd65
Igor Popov <ip-adsense@yandex.ru>
2026-03-04 13:22:21 +0000

    fix2 k8s_cluster to k8s-cluster

--- infra/provider.tf
+++ infra/provider.tf
@@ -44,14 +44,14 @@ data "yandex_client_config" "client" {}
 
 provider "helm" {
   kubernetes {
-    host                   = yandex_kubernetes_cluster.k8s_cluster.master[0].external_v4_endpoint
-    cluster_ca_certificate = yandex_kubernetes_cluster.k8s_cluster.master[0].cluster_ca_certificate
+    host                   = yandex_kubernetes_cluster.k8s-cluster.master[0].external_v4_endpoint
+    cluster_ca_certificate = yandex_kubernetes_cluster.k8s-cluster.master[0].cluster_ca_certificate
     token                  = data.yandex_client_config.client.iam_token
   }
 }
 
 provider "kubernetes" {
-  host                   = yandex_kubernetes_cluster.k8s_cluster.master[0].external_v4_endpoint
-  cluster_ca_certificate = yandex_kubernetes_cluster.k8s_cluster.master[0].cluster_ca_certificate
+  host                   = yandex_kubernetes_cluster.k8s-cluster.master[0].external_v4_endpoint
+  cluster_ca_certificate = yandex_kubernetes_cluster.k8s-cluster.master[0].cluster_ca_certificate
   token                  = data.yandex_client_config.client.iam_token
 }
