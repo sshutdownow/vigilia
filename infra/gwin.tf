a5d70109b661e8e00270eaa6b801d4f73c66f04b
Igor Popov <ip-adsense@yandex.ru>
2026-03-04 14:29:42 +0000

    fix gwin

--- infra/gwin.tf
+++ infra/gwin.tf
@@ -1,20 +1,20 @@
 resource "helm_release" "gwin" {
-  name       = "gwin"
-  repository = "oci://cr.yandex/yc-marketplace/yandex-cloud/gwin/gwin-chart"
-  version    = "v1.0.10"
-  chart      = "gwin-ingress-controller"
-  namespace  = "kube-system"
+  name             = "gwin"
+  repository       = "oci://cr.yandex/yc-marketplace/yandex-cloud/gwin/gwin-chart"
+  version          = "v1.0.10"
+  chart            = "gwin-ingress-controller"
+  namespace        = "gwin-ns"
+  create_namespace = true
 
-  set = [
-    {
-      name  = "folderId"
-      value = var.folder_id
-    },
-    {
-      name  = "clusterId"
-      value = yandex_kubernetes_cluster.k8s-cluster.id
-    }
-  ]
+  set {
+    name  = "controller.folderId"
+    value = var.folder_id
+  }
+
+  set_file {
+    name  = "controller.ycServiceAccount.secret.value"
+    value = "authorized_key.json"
+  }
 
   depends_on = [yandex_kubernetes_node_group.k8s-node-group]
 }
@@ -29,5 +29,5 @@ resource "yandex_resourcemanager_folder_iam_member" "gwin_roles" {
   ])
   folder_id = var.folder_id
   role      = each.key
-  member    = "serviceAccount:${data.yandex_iam_service_account.k8s_sa.id}"
+  member    = "serviceAccount:${yandex_iam_service_account.k8s_sa.id}"
 }
