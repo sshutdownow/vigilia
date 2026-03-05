2b2a37c4ebb0da373ef30c9ac735d661d6e17df6
Igor Popov <ip-adsense@yandex.ru>
2026-03-05 12:02:39 +0000

    gwin rewrite

---
+++ infra/argocs-app.tf
@@ -0,0 +1,54 @@
+resource "helm_release" "sausage_store_app" {
+  count      = false # TODO
+  name       = "sausage-store-app"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argocd-apps"
+  namespace  = "argocd"
+
+  values = [
+    yamlencode({
+      applications = [
+        {
+          name      = "sausage-store"
+          namespace = "argocd"
+          project   = "default"
+          source = {
+            # Pulls from OCI instead of Git
+            repoURL        = "cr.yandex/${yandex_container_registry.container-registry.id}"
+            chart          = "sausage-store"
+            targetRevision = "1.0.0"
+            helm = {
+              values = yamlencode({
+                backend = {
+                  image = "cr.yandex/${yandex_container_registry.container-registry.id}/sausage-backend"
+                }
+                frontend = {
+                  image = "cr.yandex/${yandex_container_registry.container-registry.id}/sausage-frontend"
+                }
+                ingress = {
+                  annotations = {
+                    "gwin.ingress.kubernetes.io/certificate-id" = yandex_cm_certificate.le_cert.id
+                  }
+                }
+              })
+            }
+          }
+          destination = {
+            server    = "https://kubernetes.default.svc"
+            namespace = "sausage-store"
+          }
+          syncPolicy = {
+            automated = { prune = true, selfHeal = true }
+            syncOptions = ["CreateNamespace=true"]
+          }
+        }
+      ]
+    })
+  ]
+
+  depends_on = [
+    yandex_kubernetes_cluster.k8s-cluster,
+    helm_release.argocd,
+    kubernetes_secret_v1.yc_registry_oci
+  ]
+}
--- infra/gwin.tf
+++ infra/gwin.tf
@@ -1,38 +1,9 @@
-resource "helm_release" "gwin" {
-  name             = "gwin"
-  repository       = "oci://cr.yandex/yc-marketplace/yandex-cloud/gwin"
-  version          = "v1.0.10"
-  chart            = "gwin-chart"
-  namespace        = "gwin-ns"
-  create_namespace = true
-
-  #set = [
-  #  {
-  #    name  = "controller.folderId"
-  #    value = var.folder_id
-  #  },
-  #  {
-  #    name  = "controller.ycServiceAccount.secret.value"
-  #    value = file("authorized_key.json")
-  #  }
-  #]
-
-  values = [
-    yamlencode({
-      controller = {
-        folderId = var.folder_id
-        ycServiceAccount = {
-          secret = {
-            value = file("authorized_key.json")
-          }
-        }
-      }
-    })
-  ]
-
-  depends_on = [yandex_kubernetes_node_group.k8s-node-group]
+# Сервисный аккаунт для Gwin
+resource "yandex_iam_service_account" "gwin_sa" {
+  name = "gwin-sa"
 }
 
+# Назначение ролей
 resource "yandex_resourcemanager_folder_iam_member" "gwin_roles" {
   for_each = toset([
     "alb.editor",
@@ -43,5 +14,40 @@ resource "yandex_resourcemanager_folder_iam_member" "gwin_roles" {
   ])
   folder_id = var.folder_id
   role      = each.key
-  member    = "serviceAccount:${yandex_iam_service_account.k8s-sa.id}"
+  member    = "serviceAccount:${yandex_iam_service_account.gwin_sa.id}"
+}
+
+resource "helm_release" "gwin" {
+  name             = "gwin"
+  repository       = "oci://cr.yandex/yc-marketplace/yandex-cloud/gwin"
+  version          = "v1.0.10"
+  chart            = "gwin-chart"
+  namespace        = "gwin-ns"
+  create_namespace = true
+
+  set = [
+    {
+      name  = "serviceAccount.id"
+      value = yandex_iam_service_account.gwin_sa.id
+    },
+    {
+      name  = "controller.folderId"
+      value = var.folder_id
+    }
+  ]
+
+  depends_on = [
+    yandex_kubernetes_node_group.k8s-node-group,
+    yandex_resourcemanager_folder_iam_member.gwin_roles
+  ]
+}
+
+resource "kubernetes_manifest" "gateway_class" {
+  manifest = {
+    apiVersion = "gateway.networking.k8s.io/v1"
+    kind       = "GatewayClass"
+    metadata   = { name = "yc-l7-gw" }
+    spec       = { controllerName = "gateway.yc.io/gwin" }
+  }
+  depends_on = [helm_release.gwin]
 }
