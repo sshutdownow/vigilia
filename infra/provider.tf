8d4feaee4bc6e7476fb47a5c492af1cd3cf7a597
Igor Popov <ip-adsense@yandex.ru>
2026-03-05 13:32:06 +0000

    remove base64decode for k8s cert

--- infra/provider.tf
+++ infra/provider.tf
@@ -45,13 +45,13 @@ data "yandex_client_config" "client" {}
 provider "helm" {
   kubernetes = {
     host                   = yandex_kubernetes_cluster.k8s-cluster.master[0].external_v4_endpoint
-    cluster_ca_certificate = base64decode(yandex_kubernetes_cluster.k8s-cluster.master[0].cluster_ca_certificate)
+    cluster_ca_certificate = yandex_kubernetes_cluster.k8s-cluster.master[0].cluster_ca_certificate
     token                  = data.yandex_client_config.client.iam_token
   }
 }
 
 provider "kubernetes" {
   host                   = yandex_kubernetes_cluster.k8s-cluster.master[0].external_v4_endpoint
-  cluster_ca_certificate = base64decode(yandex_kubernetes_cluster.k8s-cluster.master[0].cluster_ca_certificate)
+  cluster_ca_certificate = yandex_kubernetes_cluster.k8s-cluster.master[0].cluster_ca_certificate
   token                  = data.yandex_client_config.client.iam_token
 }
