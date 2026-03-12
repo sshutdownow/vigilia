497a5549fa4f76f4cdc3be59d4b502e9077c31ab
Igor Popov <ip-adsense@yandex.ru>
2026-03-12 10:37:39 +0000

    fix rescource to data

--- infra/argocd.tf
+++ infra/argocd.tf
@@ -54,7 +54,7 @@ resource "helm_release" "argocd" {
                 "gwin.yandex.cloud/groupName"           = "ingress"
 #                "gwin.yandex.cloud/subnets"             = yandex_vpc_subnet.subnet-a.id
                 "gwin.yandex.cloud/externalIPv4Address" = yandex_vpc_address.gwin_static_ip.external_ipv4_address[0].address
-                "gwin.yandex.cloud/certificateId"       = yandex_cm_certificate.le_cert.id
+                "gwin.yandex.cloud/certificateId"       = data.yandex_cm_certificate.le_cert.id
                 "gwin.yandex.cloud/securityGroups"      = yandex_vpc_security_group.gwin.id
                 "gwin.yandex.cloud/backend-protocol"    = "http"
                 "gwin.yandex.cloud/redirect.argo-redirect.replaceScheme" = "https"
@@ -79,7 +79,7 @@ resource "helm_release" "argocd" {
               }]
               tls = [{
                 hosts = ["argocd.${var.domain_name}"]
-                secretName = "yc-certmgr-cert-id-${yandex_cm_certificate.le_cert.id}"
+                secretName = "yc-certmgr-cert-id-${data.yandex_cm_certificate.le_cert.id}"
               }]
             }
           }
--- infra/dns.tf
+++ infra/dns.tf
@@ -3,42 +3,9 @@ data "yandex_dns_zone" "sausage_store_public_zone" {
   name = "vigilia-site"
 }
 
-# Запрос на управляемый сертификат Let's Encrypt
-resource "yandex_cm_certificate" "le_cert" {
-  name      = "sausage-store-le"
-  folder_id = var.folder_id
-  domains   = [
-    var.domain_name,
-    "*.${var.domain_name}"
-  ]
-
-  managed {
-    challenge_type = "DNS_CNAME" # Самый надежный способ для автоматизации
-  }
-
-  lifecycle {
-    # Запрещает удаление сертификата через terraform destroy
-    prevent_destroy = true
-
-    # Игнорирует изменения в доменах, чтобы не инициировать перевыпуск
-    ignore_changes = [domains, name]
-  }
-}
-
-# Terraform берет данные из запроса сертификата и создает запись в DNS
-resource "yandex_dns_recordset" "validation_record" {
-  zone_id = data.yandex_dns_zone.sausage_store_public_zone.id
-  name    = yandex_cm_certificate.le_cert.challenges[0].dns_name
-  type    = yandex_cm_certificate.le_cert.challenges[0].dns_type
-  data    = [yandex_cm_certificate.le_cert.challenges[0].dns_value]
-  ttl     = 60
-}
-
-# АВТОМАТИЧЕСКАЯ ВАЛИДАЦИЯ
-data "yandex_cm_certificate" "vigilia-site" {
-  depends_on      = [yandex_dns_recordset.validation_record]
-  certificate_id  = yandex_cm_certificate.le_cert.id
-  wait_validation = true
+# Существующий сертификат Let's Encrypt
+data "yandex_cm_certificate" "le_cert" {
+  name = "sausage-store-le"
 }
 
 resource "yandex_dns_recordset" "observability_records" {
