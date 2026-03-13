96a0c41ccc4735959520f0aaa57a2cd17bc823b0
Igor Popov <ip-adsense@yandex.ru>
2026-03-13 07:59:04 +0000

    rename log group to default

--- infra/logging.tf
+++ infra/logging.tf
@@ -1,5 +1,8 @@
 resource "yandex_logging_group" "log_group_main" {
-  name             = "main-log-group"
+  # Если создать лог-группу с именем default,
+  # она будет лог-группой по умолчанию для того каталога,
+  # в котором создана
+  name             = "default"
   folder_id        = var.folder_id
   retention_period = "36h"
 }
