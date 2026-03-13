3d8840f043f6bbc383a6ceba9963702eec5a3eff
Igor Popov <ip-adsense@yandex.ru>
2026-03-13 17:20:52 +0000

    change argocd app

--- infra/argocs-app.tf
+++ infra/argocs-app.tf
@@ -10,57 +10,19 @@ resource "helm_release" "sausage_store_app" {
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argocd-apps"
   namespace  = "argocd"
-
+  version    = "1.0.161"
+  
   values = [
-    yamlencode({
-      applications = [
-        {
-          name      = "sausage-store"
-          namespace = "argocd"
-          project   = "default"
-          source = {
-            repoURL        = "oci://${var.gitlab_helm_url}"
-            chart          = "sausage-store"
-            targetRevision = "1.0.161"
-            helm = {
-              values = <<-EOT
-                global:
-                  imagePullSecrets:
-                  - name: gitlab-pull-secret
-
-                backend:
-                  image: ${var.gitlab_image_url}/sausage-backend
-
-                frontend:
-                  image: ${var.gitlab_image_url}/sausage-frontend
-
-                backend-report:
-                  image: ${var.gitlab_image_url}/sausage-backend-report
-
-                ingress:
-                  annotations:
-                    gwin.yandex.cloud/certificateId: ${data.yandex_cm_certificate.le_cert.id}
-              EOT
-            }
-          }
-          destination = {
-            server    = "https://kubernetes.default.svc"
-            namespace = "sausage-store"
-          }
-          syncPolicy = {
-            automated = {
-              prune    = true
-              selfHeal = true
-            }
-            syncOptions = ["CreateNamespace=true"]
-          }
-        }
-      ]
+    templatefile("${path.module}/argocd-app-values.yaml", {
+      gitlab_helm_url  = var.gitlab_helm_url
+      gitlab_image_url = var.gitlab_image_url
+      certificate_id   = data.yandex_cm_certificate.le_cert.id
     })
   ]
 
   depends_on = [
     helm_release.argocd,
+    kubernetes_namespace_v1.sausage_store,
     kubernetes_secret_v1.sausage_repo_gitlab,
     kubernetes_secret_v1.sausage_helm_gitlab,
     kubernetes_secret_v1.gitlab_pull_secret
