ed718e26cf25d2e4468ecae605def88ac9bf0834
Igor Popov <ip-adsense@yandex.ru>
2026-03-13 17:11:40 +0000

    change resource "helm_release" "sausage_store_app"

--- infra/argocs-app.tf
+++ infra/argocs-app.tf
@@ -5,7 +5,7 @@ resource "kubernetes_namespace_v1" "sausage_store" {
 }
 
 resource "helm_release" "sausage_store_app" {
-  count = 1
+  count      = 1
   name       = "sausage-store-app"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argocd-apps"
@@ -13,51 +13,49 @@ resource "helm_release" "sausage_store_app" {
 
   values = [
     yamlencode({
-      applications = [{
-        name      = "sausage-store"
-        namespace = "argocd"
-        project   = "default"
-        source = {
-          # Репозиторий чарта (OCI)
-          repoURL        = "oci://${var.gitlab_helm_url}"
-          path           = "sausage-store"
-          chart          = "sausage-store"
-          targetRevision = "1.0.161" # Версия чарта из CI
-          
-          helm = {
-            values = yamlencode({
-              global = {
-                # Секрет для скачивания образов нодами
-                imagePullSecrets = [{ name = "gitlab-pull-secret" }]
-              }
+      applications = [
+        {
+          name      = "sausage-store"
+          namespace = "argocd"
+          project   = "default"
+          source = {
+            repoURL        = "oci://${var.gitlab_helm_url}"
+            chart          = "sausage-store"
+            targetRevision = "1.0.161"
+            helm = {
+              values = <<-EOT
+                global:
+                  imagePullSecrets:
+                  - name: gitlab-pull-secret
 
-              backend = {
-                image = "${var.gitlab_image_url}/sausage-backend"
-              }
-              frontend = {
-                image = "${var.gitlab_image_url}/sausage-frontend"
-              }
-              backend-report = {
-                image = "${var.gitlab_image_url}/sausage-backend-report"
-              }
+                backend:
+                  image: ${var.gitlab_image_url}/sausage-backend
 
-              ingress = {
-                annotations = {
-                  "gwin.yandex.cloud/certificateId" = data.yandex_cm_certificate.le_cert.id
-                }
-              }
-            })
+                frontend:
+                  image: ${var.gitlab_image_url}/sausage-frontend
+
+                backend-report:
+                  image: ${var.gitlab_image_url}/sausage-backend-report
+
+                ingress:
+                  annotations:
+                    gwin.yandex.cloud/certificateId: ${data.yandex_cm_certificate.le_cert.id}
+              EOT
+            }
+          }
+          destination = {
+            server    = "https://kubernetes.default.svc"
+            namespace = "sausage-store"
+          }
+          syncPolicy = {
+            automated = {
+              prune    = true
+              selfHeal = true
+            }
+            syncOptions = ["CreateNamespace=true"]
           }
         }
-        destination = {
-          server    = "https://kubernetes.default.svc"
-          namespace = "sausage-store"
-        }
-        syncPolicy = {
-          automated   = { prune = true, selfHeal = true }
-          syncOptions = ["CreateNamespace=true"]
-        }
-      }]
+      ]
     })
   ]
 
