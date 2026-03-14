2cded1fbff2c9af58e0376def08f3dadcdb71329
Igor Popov <ip-adsense@yandex.ru>
2026-03-14 12:26:29 +0000

    another approach resource "kubernetes_manifest" "sausage_store_app"

--- infra/argocs-app.tf
+++ infra/argocs-app.tf
@@ -4,40 +4,37 @@ resource "kubernetes_namespace_v1" "sausage_store" {
   }
 }
 
-resource "helm_release" "sausage_store_app" {
-  name       = "sausage-store-app"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argocd-apps"
-  namespace  = "argocd"
-
-  values = [
-    yamlencode({
-      applications = [{
-        name      = "sausage-store"
-        namespace = "argocd"
-        project   = "default"
-        source = {
-          repoURL        = "${var.gitlab_git_url}"
-          path           = "deploy-yandex-cloud" 
-          targetRevision = "master"
-          helm = {
-            valueFiles = ["values.yaml"]
-          }
+resource "kubernetes_manifest" "sausage_store_app" {
+  manifest = {
+    "apiVersion" = "argoproj.io/v1alpha1"
+    "kind"       = "Application"
+    "metadata" = {
+      "name"      = "sausage-store"
+      "namespace" = "argocd"
+    }
+    "spec" = {
+      "project" = "default"
+      "source" = {
+        "repoURL"        = "${var.gitlab_git_url}"
+        "path"           = "deploy-yandex-cloud"
+        "targetRevision" = "master"
+        "helm" = {
+          "valueFiles" = ["values.yaml"]
         }
-        destination = {
-          server    = "https://kubernetes.default.svc"
-          namespace = "sausage-store"
+      }
+      "destination" = {
+        "server"    = "https://kubernetes.default.svc"
+        "namespace" = "sausage-store"
+      }
+      "syncPolicy" = {
+        "automated" = {
+          "prune"    = true
+          "selfHeal" = true
         }
-        syncPolicy = {
-          automated = {
-            "prune" = true
-            "selfHeal" = true
-          }
-          syncOptions = ["CreateNamespace=true"]
-        }
-      }]
-    })
-  ]
+        "syncOptions" = ["CreateNamespace=true"]
+      }
+    }
+  }
 
   depends_on = [
     helm_release.argocd,
