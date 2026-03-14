27f02b10e1fbea97b9dc78bbd894085fd1ac8b13
Igor Popov <ip-adsense@yandex.ru>
2026-03-14 19:00:53 +0000

    add ingress.yaml

--- infra/provider.tf
+++ infra/provider.tf
@@ -58,7 +58,4 @@ provider "kubernetes" {
   host                   = yandex_kubernetes_cluster.k8s-cluster.master[0].external_v4_endpoint
   cluster_ca_certificate = yandex_kubernetes_cluster.k8s-cluster.master[0].cluster_ca_certificate
   token                  = data.yandex_client_config.client.iam_token
-  experiments {
-    manifest_resource = true
-  }
 }
