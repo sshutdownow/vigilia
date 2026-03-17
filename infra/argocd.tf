334624c62c2c3ec0e4f2c4e979619e8270ef4a1f
Igor Popov <ip-adsense@yandex.ru>
2026-03-17 12:58:55 +0000

    add deps for resource "kubernetes_namespace_v1" "sausage_store" from argocd

--- infra/argocd.tf
+++ infra/argocd.tf
@@ -2,31 +2,6 @@ resource "bcrypt_hash" "argocd_password" {
   cleartext = var.argocd_admin_password
 }
 
-resource "kubernetes_namespace_v1" "sausage_store" {
-  metadata {
-    name = "sausage-store"
-  }
-}
-
-# login/password for k8s to download images from GitLab 
-resource "kubernetes_secret_v1" "gitlab_pull_secret" {
-  metadata {
-    name      = "gitlab-pull-secret"
-    # namespace = "sausage-store"
-    namespace = kubernetes_namespace_v1.sausage_store.metadata[0].name
-  }
-  type = "kubernetes.io/dockerconfigjson"
-  data = {
-    ".dockerconfigjson" = jsonencode({
-      auths = {
-        (replace(var.gitlab_image_url, "/^https?:\\/\\/|(\\/.*)$/", "")) = {
-          auth = base64encode("${var.gitlab_username}:${var.gitlab_token}")
-        }
-      }
-    })
-  }
-}
-
 resource "helm_release" "argocd" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
@@ -195,4 +170,30 @@ resource "helm_release" "argocd_apps" {
     helm_release.argocd,
     yandex_lockbox_secret_version.v1
   ]
+}
+
+resource "kubernetes_namespace_v1" "sausage_store" {
+  metadata {
+    name = "sausage-store"
+  }
+    depends_on = [helm_release.argocd]
+}
+
+# login/password for k8s to download images from GitLab 
+resource "kubernetes_secret_v1" "gitlab_pull_secret" {
+  metadata {
+    name      = "gitlab-pull-secret"
+    # namespace = "sausage-store"
+    namespace = kubernetes_namespace_v1.sausage_store.metadata[0].name
+  }
+  type = "kubernetes.io/dockerconfigjson"
+  data = {
+    ".dockerconfigjson" = jsonencode({
+      auths = {
+        (replace(var.gitlab_image_url, "/^https?:\\/\\/|(\\/.*)$/", "")) = {
+          auth = base64encode("${var.gitlab_username}:${var.gitlab_token}")
+        }
+      }
+    })
+  }
 }
