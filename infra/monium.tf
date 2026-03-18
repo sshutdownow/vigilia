32b73e4713aac63382bab4bb8e2dcb7e0530d0a4
Igor Popov <ip-adsense@yandex.ru>
2026-03-18 15:49:00 +0000

    count create array int TF

--- infra/monium.tf
+++ infra/monium.tf
@@ -1,25 +1,24 @@
 variable "enable_monium_key" {
-  description = "Флаг для активации генерации API-ключа Monium"
-  type        = bool
-  default     = false
+  description = "Флаг для активации генерации API-ключа Monium"  
+  type    = bool
+  default = false
 }
 
 resource "yandex_iam_service_account" "monium_sa" {
-  count       = var.enable_monium_key ? 1 : 0  
-  name        = "sausage-monium-sa"
-  description = "Service account for OpenTelemetry Collector to write metrics to Monium"
+  count = var.enable_monium_key ? 1 : 0
+  name  = "sausage-monium-sa"
 }
 
 resource "yandex_resourcemanager_folder_iam_member" "monitoring_editor" {
   count     = var.enable_monium_key ? 1 : 0
   folder_id = var.folder_id
   role      = "monitoring.editor"
-  member    = "serviceAccount:${yandex_iam_service_account.monium_sa.id}"
+  member    = "serviceAccount:${yandex_iam_service_account.monium_sa[0].id}"
 }
 
 resource "yandex_iam_service_account_api_key" "monium_key" {
   count              = var.enable_monium_key ? 1 : 0
-  service_account_id = yandex_iam_service_account.monium_sa.id
+  service_account_id = yandex_iam_service_account.monium_sa[0].id
 }
 
 output "monium_api_key" {
