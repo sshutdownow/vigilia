945d0d0b1cc685bd9676de3f0512388e49080816
Igor Popov <ip-adsense@yandex.ru>
2026-03-19 10:47:17 +0000

    add scopes to service API key

--- infra/monium.tf
+++ infra/monium.tf
@@ -9,16 +9,18 @@ resource "yandex_iam_service_account" "monium_sa" {
   name  = "sausage-monium-sa"
 }
 
-resource "yandex_resourcemanager_folder_iam_member" "monitoring_editor" {
+resource "yandex_resourcemanager_folder_iam_member" "monium_sa_role" {
   count     = var.enable_monium_key ? 1 : 0
   folder_id = var.folder_id
-  role      = "monitoring.editor"
+  role      = "monium.telemetry.writer"
   member    = "serviceAccount:${yandex_iam_service_account.monium_sa[0].id}"
 }
 
 resource "yandex_iam_service_account_api_key" "monium_key" {
   count              = var.enable_monium_key ? 1 : 0
   service_account_id = yandex_iam_service_account.monium_sa[0].id
+  scopes = ["yc.monium.telemetry.write"]
+  depends_on = [yandex_resourcemanager_folder_iam_member.monium_sa_role]
 }
 
 output "monium_api_key" {
