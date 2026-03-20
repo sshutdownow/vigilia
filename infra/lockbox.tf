15ee319465b6b4a6e7d4dbb13dec9d5337f96a0f
Igor Popov <ip-adsense@yandex.ru>
2026-03-20 14:10:39 +0000

    add kms.keys.encrypterDecrypter to sausage_backend_sa

--- infra/lockbox.tf
+++ infra/lockbox.tf
@@ -31,7 +31,8 @@ resource "yandex_iam_service_account" "sausage_backend_sa" {
 resource "yandex_resourcemanager_folder_iam_member" "sausage_backend_roles" {
   for_each  = toset([
     "lockbox.payloadViewer", # Чтобы видеть "обертку" секрета
-    "kms.viewer"             # Чтобы расшифровать "начинку" ключом
+    "kms.viewer",             # Чтобы расшифровать "начинку" ключом
+    "kms.keys.encrypterDecrypter"
   ])
   
   folder_id = var.folder_id
