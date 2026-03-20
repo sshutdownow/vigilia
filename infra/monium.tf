1023104560d6b0a1b5be1677d853bdec6dba93fa
Igor Popov <ip-adsense@yandex.ru>
2026-03-20 22:46:17 +0000

    oetel settings

--- infra/monium.tf
+++ infra/monium.tf
@@ -19,8 +19,8 @@ resource "yandex_resourcemanager_folder_iam_member" "monium_sa_role" {
 resource "yandex_iam_service_account_api_key" "monium_key" {
   count              = var.enable_monium_key ? 1 : 0
   service_account_id = yandex_iam_service_account.monium_sa[0].id
-  scopes = ["yc.monium.telemetry.write"]
-  depends_on = [yandex_resourcemanager_folder_iam_member.monium_sa_role]
+  scopes             = ["yc.monium.telemetry.write"]
+  depends_on         = [yandex_resourcemanager_folder_iam_member.monium_sa_role]
 }
 
 output "monium_api_key" {
