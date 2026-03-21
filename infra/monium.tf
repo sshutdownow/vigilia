9133f7904b93269256dcf1cbb5ac7176effcbd17
Igor Popov <ip-adsense@yandex.ru>
2026-03-21 17:19:33 +0000

    clear monium.tf

--- infra/monium.tf
+++ infra/monium.tf
@@ -20,10 +20,9 @@ resource "yandex_iam_service_account_api_key" "monium_key" {
   count              = var.enable_monium_key ? 1 : 0
   service_account_id = yandex_iam_service_account.monium_sa[0].id
   scopes             = ["yc.monium.telemetry.write"]
-  depends_on         = [yandex_resourcemanager_folder_iam_member.monium_sa_role]
 }
 
 output "monium_api_key" {
-  value     = length(yandex_iam_service_account_api_key.monium_key) > 0 ? yandex_iam_service_account_api_key.monium_key[0].secret_key : ""
+  value     = one(yandex_iam_service_account_api_key.monium_key[*].secret_key)
   sensitive = true
 }
