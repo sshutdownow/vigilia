19ab260df61dc99fbe841131b24b0fc0de2bf622
Igor Popov <ip-adsense@yandex.ru>
2026-04-17 15:17:37 +0000

    remove commented lines

--- infra/provider.tf
+++ infra/provider.tf
@@ -24,19 +24,7 @@ terraform {
     }
   }
 
-  #
-  # https://docs.gitlab.com/ci/variables/predefined_variables/
-  # CI_PROJECT_ID - The ID of the current project. This ID is unique across all projects on the GitLab instance.
-  #
   backend "http" {
-/*     address        = "https://cloud-services-engineer.gitlab.yandexcloud.net/api/v4/projects/${CI_PROJECT_ID}/terraform/state/tfstate"
-    lock_address   = "https://cloud-services-engineer.gitlab.yandexcloud.net/api/v4/projects/${CI_PROJECT_ID}/terraform/state/tfstate/lock"
-    unlock_address = "https://cloud-services-engineer.gitlab.yandexcloud.net/api/v4/projects/${CI_PROJECT_ID}/terraform/state/tfstate/lock"
-    lock_method    = "POST"
-    unlock_method  = "DELETE"
-    retry_wait_min = 5
-    username       = "${GITLAB_USER_NAME}"
-    password       = "${TF_HTTP_PASSWORD}" */
   }
 }
 
