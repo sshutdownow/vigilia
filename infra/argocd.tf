0b39562fcbf9afefb52431dd0d0b02e67bcca8bb
Igor Popov <ip-adsense@yandex.ru>
2026-05-10 08:40:53 +0000

    fix argo ver "v3.4.1"

--- infra/argocd.tf
+++ infra/argocd.tf
@@ -10,6 +10,7 @@ resource "helm_release" "argocd" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
+  version          = "v3.4.1"
   namespace        = "argocd"
   create_namespace = true
 
