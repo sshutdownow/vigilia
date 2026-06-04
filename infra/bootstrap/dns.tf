17e8c646c5004d18c843dd344363c33c8732d92e
Igor Popov <ip-adsense@yandex.ru>
2026-06-04 12:41:28 +0000

    revert dns data get by name

--- infra/bootstrap/dns.tf
+++ infra/bootstrap/dns.tf
@@ -1,6 +1,6 @@
 # Создаём публичную DNS-зону
 resource "yandex_dns_zone" "sausage_store_public_zone" {
-  name   = "observability"
+  name   = "observability.${var.domain_name}"
   zone   = "${var.domain_name}." # точка обязательна
   public = true
 }
